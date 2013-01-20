bb81f5e1d0a8a5173ee3bbc4b9a254c82abbe37a
Alain Hernandez <aiyoku19@gmail.com>
2013-01-19 20:36:44 -0500

    Implements Dropdown Navigation
    Still haven't completeted listener. Need add fragment or activity.

--- src/io/doortags/android/MessagesActivity.java
+++ src/io/doortags/android/MessagesActivity.java
@@ -1,7 +1,13 @@
 package io.doortags.android;
 
+import android.app.ActionBar;
+import android.app.ActionBar.OnNavigationListener;
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
 
 public class MessagesActivity extends Activity {
     /**
@@ -11,5 +17,35 @@ public class MessagesActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        
+        SpinnerAdapter mSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.action_list,
+                android.R.layout.simple_spinner_dropdown_item);
+        
+        OnNavigationListener mNavigationCallback = new OnNavigationListener() {
+        	  // Get the same strings provided for the drop-down's ArrayAdapter
+        	  String[] strings = getResources().getStringArray(R.array.action_list);
+
+        	  @Override
+        	  public boolean onNavigationItemSelected(int position, long itemId) {
+        	    // Create new fragment from our own Fragment class
+/*        	    ListContentFragment newFragment = new ListContentFragment();
+        	    FragmentManager fragmentManager = getFragmentManager();
+        	    FragmentTransaction ft = fragmentManager.beginTransaction();
+        	    // Replace whatever is in the fragment container with this fragment
+        	    //  and give the fragment a tag name equal to the string at the position selected
+        	    ft.replace(R.array.action_list, newFragment, strings[position]);
+        	    // Apply changes
+        	    ft.commit();*/
+        	    return true;
+        	  }
+        	};
+        
+        actionBar.setListNavigationCallbacks(mSpinnerAdapter, mNavigationCallback);
+        
+        
+        
+        
     }
 }
