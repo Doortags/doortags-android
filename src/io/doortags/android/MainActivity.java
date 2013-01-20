6f206b364b21f0edda3f6fa4bb4cf89b4ce92822
Alain Hernandez <aiyoku19@gmail.com>
2013-01-20 05:19:13 -0500

    Implemented Logo, NFC Read/Write
    Using custom MIME type in this iteration

--- src/io/doortags/android/MainActivity.java
+++ src/io/doortags/android/MainActivity.java
@@ -4,15 +4,24 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.SpinnerAdapter;
+import android.widget.Toast;
+import io.doortags.android.utils.Utils;
+import org.danielge.nfcskeleton.NdefReaderActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends NdefReaderActivity {
     private static final int POS_RING = 0,
                              POS_MANAGE = 1;
     private static final String RING_ID = "ring",
                                 MANAGE_ID = "manage";
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     /**
      * Called when the activity is first created.
@@ -63,5 +72,31 @@ public class MainActivity extends Activity {
         };
 
         actionBar.setListNavigationCallbacks(mSpinnerAdapter, mNavigationCallback);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter[] filter = new IntentFilter[1];
+        filter[0] = buildIntentFilter();
+        enableReadTagMode(filter);
+    }
+    private IntentFilter buildIntentFilter() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filter.addDataType(Utils.MIME);
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            Log.wtf(TAG, "Invalid MIME type", e);
+            Toast.makeText(this, "Something went wrong. Please contact your local developer",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        return filter;
+    }
+    @Override
+    protected void onNdefMessage(NdefMessage ndefMessage) {
+        Toast.makeText(this.getApplicationContext(), "Detected Tag" + ndefMessage.toString() , Toast.LENGTH_LONG).show();
     }
 }
--- src/io/doortags/android/WriteTagActivity.java
+++ src/io/doortags/android/WriteTagActivity.java
@@ -1,8 +1,17 @@
 package io.doortags.android;
 
-import android.nfc.Tag;
+import android.content.Intent;
+import android.nfc.*;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import io.doortags.android.utils.Utils;
+import org.danielge.nfcskeleton.NfcUtils;
 import org.danielge.nfcskeleton.NfcWriterActivity;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Locale;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,10 +23,83 @@ import org.danielge.nfcskeleton.NfcWriterActivity;
 public class WriteTagActivity extends NfcWriterActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.nfc_write);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        enableWriteTagMode();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        disableWriteTagMode();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        String action = intent.getAction();
+        setIntent(intent);
+
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(getIntent().getAction())) {
+            Log.i(TAG, "Discovered tag");
+
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            onTagDiscovered(tag);
+        }
+    }
+    private NdefMessage prepareMessage (int id) {
+        NdefRecord record = new NdefRecord(
+                NdefRecord.TNF_MIME_MEDIA,
+                Utils.MIME.getBytes(Charset.forName("US-ASCII")),
+                new byte[0],
+                String.valueOf(id).getBytes(Charset.forName("US-ASCII"))
+        );
+
+        return new NdefMessage(new NdefRecord[]{record});
+    }
+
+    public NdefMessage createTextMessage(String payload, Locale locale, boolean encodeInUtf8) {
+        byte[] langBytes = locale.getLanguage().getBytes(Charset.forName("US-ASCII"));
+        Charset utfEncoding = encodeInUtf8 ? Charset.forName("UTF-8") : Charset.forName("UTF-16");
+        byte[] textBytes = payload.getBytes(utfEncoding);
+        int utfBit = encodeInUtf8 ? 0 : (1 << 7);
+        char status = (char) (utfBit + langBytes.length);
+        byte[] data = new byte[1 + langBytes.length + textBytes.length];
+        data[0] = (byte) status;
+        System.arraycopy(langBytes, 0, data, 1, langBytes.length);
+        System.arraycopy(textBytes, 0, data, 1 + langBytes.length, textBytes.length);
+        NdefRecord record = new NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+                NdefRecord.RTD_TEXT, new byte[0], data);
+        return new NdefMessage(new NdefRecord[]{record});
     }
 
     @Override
     protected void onTagDiscovered(Tag tag) {
-        throw new UnsupportedOperationException();
+
+        Toast.makeText(this.getApplicationContext(), "Detected Tag", Toast.LENGTH_SHORT).show();
+
+        try {
+            NdefMessage msgNFC = prepareMessage(8);
+            //createTextMessage("test",Locale.ENGLISH,true)
+            NfcUtils.writeNdefTag(msgNFC, tag);
+            Toast.makeText(this.getApplicationContext(), "Successfully wrote to Tag", Toast.LENGTH_SHORT).show();
+        }
+        catch (FormatException e) {
+            Log.i("WriteToNFC", "Writing to NDEF failed in WriteToNFC");
+        }
+        catch (NfcUtils.TagNotWritableException e) {
+            Log.i("WriteToNFC", e.toString());
+        }
+        catch (NfcUtils.NdefMessageTooLongException e) {
+            Log.i("WriteToNFC", "e");
+        } catch (IOException e) {
+            Log.i("WriteToNFC", "");
+        }
+
     }
 }
