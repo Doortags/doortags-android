d4a65bcd03e59ef0e4db5582c2b6132743e9d098
Daniel Ge <dange@seas.upenn.edu>
2013-02-02 15:34:05 -0500

    Remove Cancel button from progress dialog

--- src/io/doortags/android/SendProgressFragment.java
+++ src/io/doortags/android/SendProgressFragment.java
@@ -9,8 +9,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.*;
-import android.widget.Button;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 import io.doortags.android.api.DoortagsApiClient;
 import io.doortags.android.api.DoortagsApiException;
@@ -52,20 +53,7 @@ public class SendProgressFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
             savedInstanceState) {
 
-        final View view = inflater.inflate(R.layout.sendprogress_fragment, container, false);
-        Button cancel = (Button) view.findViewById(R.id.prog_cancel);
-
-        final SendProgressFragment that = this;
-
-
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                that.getDialog().cancel();
-            }
-
-        });
-
+        View view = inflater.inflate(R.layout.sendprogress_fragment, container, false);
         return view;
     }
 
